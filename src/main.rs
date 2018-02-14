c337e3696383994a30fef25425f87dcdc4e751ca
Arian van Putten <aeroboy94@gmail.com>
2018-02-14 13:51:14 +0100

    Create instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use]
+extern crate vulkano;
+
+use vulkano::instance::layers_list;
+use vulkano::instance::Instance;
+use vulkano::instance::InstanceExtensions;
+
 fn main() {
-    println!("Hello, world!");
+    let application_info = app_info_from_cargo_toml!();
+
+    let instance_extensions = InstanceExtensions::none();
+
+    // Debug layers can be enabled with environment variables. We prefer that
+    let layers = None;
+
+    let instance = Instance::new(Some(&application_info), &instance_extensions, layers);
 }
